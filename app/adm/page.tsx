01ffc229d975976804bbfcd3e0c98569e0dbb0a8
franciel <francielmsq@gmail.com>
2026-06-06 11:43:04 -0300

    feat: ajustado pastas da api

--- app/adm/page.tsx
+++ app/adm/page.tsx
@@ -72,8 +72,8 @@ export default function AdmPage() {
 
   return (
     <div className="p-8 max-w-md mx-auto text-black">
-      <h1 className="text-2xl font-bold mb-6 text-center">Painel ADM - Cadastrar Roupa atualizado1</h1>
-      <h2>atualização 3</h2>
+      <h1 className="text-2xl font-bold mb-6 text-center">Painel ADM - Cadastrar Roupa</h1>
+      <h2>atualização 5</h2>
 
       <form onSubmit={handleUpload} className="flex flex-col gap-4 border p-6 rounded-lg bg-white shadow">
         
