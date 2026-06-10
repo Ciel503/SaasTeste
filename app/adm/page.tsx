c0b8ce58101e0cc4a81ce9bea09cca582c7dda56
franciel <francielmsq@gmail.com>
2026-06-10 17:46:02 -0300

    add retonoa pra home na pagina adm

--- app/adm/page.tsx
+++ app/adm/page.tsx
@@ -2,6 +2,7 @@
 
 import FormularioAdm from "./FormularioAdm";
 import ListaEstoque from "../../components/listaestoque"; // 🔥 Injeta a nova lista isolada
+import Link from "next/dist/client/link";
 
 export default function AdmPage() {
   return (
@@ -11,9 +12,9 @@ export default function AdmPage() {
         
         {/* Badge superior com a identidade da marca */}
         <div className="text-center mb-3">
-          <span className="text-pink-500 font-bold tracking-widest text-[10px] uppercase bg-pink-950/40 px-3 py-1 rounded-full border border-pink-500/20">
-            Painel de Controle
-          </span>
+          <h1 className="text-pink-500 font-bold tracking-widest text-[30px] uppercase bg-pink-950/40 px-3 py-1 rounded-full border border-pink-500/50">
+             <Link href="/">VÉSTIA</Link>
+          </h1>
         </div>
 
         <h1 className="text-2xl font-black mb-1 text-center uppercase tracking-tight text-white">
